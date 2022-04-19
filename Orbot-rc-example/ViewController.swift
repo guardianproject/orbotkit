98a25a8e3b0a29dcc2c63eb219a6d885b25c0eb2
Benjamin Erhart <berhart@netzarchitekten.com>
2022-04-19 16:04:57 +0200

    Initial Commit

---
+++ Orbot-rc-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Orbot-rc-example
+//
+//  Created by Benjamin Erhart on 19.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
